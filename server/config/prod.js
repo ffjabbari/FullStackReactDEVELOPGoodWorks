f81513f4df63391b5804caa36540888efe97e098
gl3127 <fjabbari@ventera.com>
2021-06-21 19:45:55 -0500

    init004

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,9 +3,11 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY,
-  redirectDomain: process.env.REDIRECT_DOMAIN
+  cookieKey: process.env.COOKIE_KEY
 };
+// module.exports = {
+//   googleClientID: '303391505387-7nssssptda6n9ekbc7pegqtge61qq58t.apps.googleusercontent.com',
+//   googleClientSecret: 'OM9kx-jhKJzBAdfKjrhCpch3',
+//   mongoURI: 'mongodb+srv://ffjabbari:15pishyy@emaily-dev.x7ckf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+//   cookieKey: 'fredman'
+// };
